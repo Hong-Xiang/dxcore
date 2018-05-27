6bfefc5eb9d5f1a6be2ee481f43f8923f07c6341
HongXiang <hx.hongxiang@gmail.com>
2018-05-27 14:54:23 +0800

    add some docs

--- src/python/dxl/core/logger/backend.py
+++ src/python/dxl/core/logger/backend.py
@@ -2,6 +2,10 @@ import logging
 
 
 class SimpleInMemoryBackend:
+    """
+    Simple in memory backend, designed to simplify tests.
+    """
+
     def __init__(self):
         self.messages = []
 
@@ -19,5 +23,15 @@ class SimpleInMemoryBackend:
     def content(self):
         return '\n'.join(self.messages)
 
+    def clear(self):
+        self.messages = []
+
     def __len__(self):
+        """
+        Returns number of messages added.
+        """
         return len(self.messages)
+
+
+# Alias of logging
+StandardBackend = logging
