3b1bdd0d5f4d1687b5d6c6e7703259b508b6c5c2
HongXiang <hx.hongxiang@gmail.com>
2018-09-03 10:53:39 +0800

    fix yaml dep

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     namespace_packages=['dxl'],
     packages=find_packages('src/python'),
     package_dir={'': 'src/python'},
-    install_requires=[],
+    install_requires=["pyyaml"],
     zip_safe=False)
