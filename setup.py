13b42ba2788c82f9534bc6f7125bfbb1277a5c34
hongjiang <hongjiangdong@outlook.com>
2018-10-16 21:02:53 +0800

    update install requires

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='dxl-core',
-    version='0.1.6',
+    version='0.1.7',
     description='Core utility library.',
     url='https://github.com/tech-pi/dxcore',
     author='Hong Xiang',
@@ -10,5 +10,10 @@ setup(
     namespace_packages=['dxl'],
     packages=find_packages('src/python'),
     package_dir={'': 'src/python'},
-    install_requires=["pyyaml"],
+    install_requires=[
+        'typing',
+        'ipython',
+        'pathlib',
+        'numpy',
+    ],
     zip_safe=False)
